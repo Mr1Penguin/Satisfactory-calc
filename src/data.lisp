50154ad918564cfc05638f3506b4f74339f6de6c
Igor Sharapov <igorsharapov@outlook.com>
2020-04-08 10:13:29 +0000

    Style

--- src/data.lisp
+++ src/data.lisp
@@ -1,33 +1,30 @@
 (defpackage satisfactory-calc/data
-  (:use :cl
-	:satisfactory-calc/types)
+  (:use :cl :satisfactory-calc/types)
   (:export :load-db))
 (in-package :satisfactory-calc/data)
 
 ;; iron ignot
 
-(defvar iron-ignot-recipe (satisfactory-calc/types::make-recipe :input (vector (cons "Iron ore" 30.0))
-				  :output "Iron ignot"
-				  :per-min 30.0))
+(defvar iron-ignot-recipe (satisfactory-calc/types::make-recipe :input '(vector (cons "Iron ore" 30.0))
+                                                                :output "Iron ignot"
+                                                                :per-min 30.0))
 (defvar iron-alloy-ignot-recipe (satisfactory-calc/types::make-recipe :input (vector (cons "Iron ore" 20.0)
-						       (cons "Copper ore" 20.0))
-					:output "Iron ignot"
-					:per-min 50.0))
+                                                                                     (cons "Copper ore" 20.0))
+                                                                      :output "Iron ignot"
+                                                                      :per-min 50.0))
 
-(defvar *default-db*
-  (list (cons :items (vector (satisfactory-calc/types::make-item :name "Iron ore")
-			     (satisfactory-calc/types::make-item :name "Copper ore")
-			     (satisfactory-calc/types::make-item :name "Crude oil")
-			     (satisfactory-calc/types::make-item :name "Coal")
-			     (satisfactory-calc/types::make-item :name "S.A.M")
-			     (satisfactory-calc/types::make-item :name "Limestone")
-			     (satisfactory-calc/types::make-item :name "Quartz")
-
-			     (satisfactory-calc/types::make-item :name "Iron ignot"
-								 :recipes (vector iron-ignot-recipe))))
-	(cons :nodes #())
-	(cons :buildings #())
-	(cons :miners-mk 1)))
+(defvar *default-db* (list (cons :items (vector (satisfactory-calc/types::make-item :name "Iron ore")
+                                                (satisfactory-calc/types::make-item :name "Copper ore")
+                                                (satisfactory-calc/types::make-item :name "Crude oil")
+                                                (satisfactory-calc/types::make-item :name "Coal")
+                                                (satisfactory-calc/types::make-item :name "S.A.M")
+                                                (satisfactory-calc/types::make-item :name "Limestone")
+                                                (satisfactory-calc/types::make-item :name "Quartz")
+                                                (satisfactory-calc/types::make-item :name "Iron ignot"
+                                                                                    :recipes (vector iron-ignot-recipe))))
+                           (cons :nodes #())
+                           (cons :buildings #())
+                           (cons :miners-mk 1)))
 
 (defvar *db* nil)
 
@@ -38,18 +35,13 @@
   (merge-pathnames basepath "db.lisp"))
 
 (defun safe-db (filename)
-  (with-open-file (out filename
-		       :direction :output
-		       :if-exists :supersede)
-    (with-standard-io-syntax
-      (print *db* out))))
+  (with-open-file (out filename :direction :output
+                                :if-exists :supersede)
+    (with-standard-io-syntax (print *db* out))))
 
 (defun load-db (filename)
-  (handler-case
-      (with-open-file (in filename
-			  :if-does-not-exist :error)
-	(with-standard-io-syntax
-	  (setf *db* (read in))))
+  (handler-case (with-open-file (in filename :if-does-not-exist :error)
+                  (with-standard-io-syntax (setf *db* (read in))))
     (file-error nil
       (setf *db* *default-db*)))
   t)
--- tests/data.lisp
+++ tests/data.lisp
@@ -5,21 +5,21 @@
 (in-package :satisfactory-calc/tests/data)
 
 (deftest load-db-return-default
-  (let ((filename "~/.scl/test.lisp"))
-    (handler-case (delete-file filename)
-      (file-error nil nil))
-    (testing "should return default db if file not accessible"
-      (ok (load-db filename))
-      (ok (equal satisfactory-calc/data::*default-db* satisfactory-calc/data::*db*)))))
+    (let ((filename "~/.scl/test.lisp"))
+      (handler-case (delete-file filename)
+        (file-error nil nil))
+      (testing "should return default db if file not accessible"
+               (ok (load-db filename))
+               (ok (equal satisfactory-calc/data::*default-db* satisfactory-calc/data::*db*)))))
 
 (deftest save-load-db
-  (testing "should make save-load cycle"
-    (let ((file (merge-pathnames (satisfactory-calc/data::create-data-directory) "test.lisp"))
-	  (data #(1 2 3)))
-      (setf satisfactory-calc/data::*db* data)
-      (satisfactory-calc/data::safe-db file)
-      (ok (probe-file file))
-      (ok (load-db file))
-      (ok (equalp data satisfactory-calc/data::*db*))
-      (handler-case (delete-file file)
-	(file-error nil nil)))))
+    (testing "should make save-load cycle"
+             (let ((file (merge-pathnames (satisfactory-calc/data::create-data-directory) "test.lisp"))
+                   (data #(1 2 3)))
+               (setf satisfactory-calc/data::*db* data)
+               (satisfactory-calc/data::safe-db file)
+               (ok (probe-file file))
+               (ok (load-db file))
+               (ok (equalp data satisfactory-calc/data::*db*))
+               (handler-case (delete-file file)
+                 (file-error nil nil)))))
